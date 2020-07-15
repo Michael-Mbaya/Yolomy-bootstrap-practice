02f5b46bdd485aae1e1af4340ed6361b645dbe47
Michael-Mbaya <gikundamike@gmail.com>
2020-07-15 14:39:42 +0300

    about us borders

---
+++ index.js
@@ -0,0 +1,22 @@
+num1=5
+num2=10
+
+
+function add(num1,num2){
+    return num1+num2
+}
+
+
+var num1=2
+var num2=4
+
+
+add(2,5)//pass by value
+add(num1,num2)//pass by reference
+
+// pass by referrence 
+// pass by value
+
+//declaration vs definition
+// interpreted programming language
+//runtime vs compile time execution
